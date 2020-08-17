52dd9f0a4c173280c5c23a024f9aa1e3b819823d
Andra Antariksa <andra.antariksa@gmail.com>
2020-08-17 15:12:34 +0700

    update Travis CI g++ version

---
+++ script/install_deps.sh
@@ -0,0 +1,13 @@
+# https://gist.github.com/Cartexius/4c437c084d6e388288201aadf9c8cdd5#gistcomment-2651297
+sudo apt-get install libgtest-dev
+cd /usr/src/googletest/googletest
+sudo mkdir build
+cd build
+sudo cmake ..
+sudo make
+sudo cp libgtest* /usr/lib/
+cd ..
+sudo rm -rf build
+sudo mkdir /usr/local/lib/googletest
+sudo ln -s /usr/lib/libgtest.a /usr/local/lib/googletest/libgtest.a
+sudo ln -s /usr/lib/libgtest_main.a /usr/local/lib/googletest/libgtest_main.a
